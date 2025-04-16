1c13130fe25ce00b6f470e77ce427372bdd5cacb
Anish <anish2shukla@gmail.com>
2025-04-16 21:33:56 +0530

    added and tested using pwoershell

--- copyFile.py
+++ copyFile.py
@@ -4,12 +4,9 @@ import sys
 from datetime import datetime
 
 def backup_files(source_directory, dest_directory):
-    # Check if source directory exists
     if not os.path.isdir(source_directory):
         print("Source directory does not exist.")
         return
-
-    # Check if destination directory exists
     if not os.path.isdir(dest_directory):
         print("Destination directory does not exist.")
         return
@@ -20,9 +17,7 @@ def backup_files(source_directory, dest_directory):
         if os.path.isfile(source_file):
             dest_file = os.path.join(dest_directory, filename)
 
-            # Check if file already exists in destination
             if os.path.exists(dest_file):
-                # Append timestamp to file name before extension
                 name, ext = os.path.splitext(filename)
                 timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
                 new_filename = "{name}_{timestamp}{ext}"
@@ -38,6 +33,6 @@ if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("provide files with source and destination directry")
     else:
-        source = r"C:\Users\Anish\OneDrive\Desktop\folder1"
-        destination = r"C:\Users\Anish\OneDrive\Desktop\backup"
+        source = r"C:\Users\Anish\OneDrive\Desktop\folder1" #tseting with local folder path, also using r for absolute path, Tested using powershell
+        destination = r"C:\Users\Anish\OneDrive\Desktop\backup" #tseting with local folder path
         backup_files(source, destination)
